058617af1a1d44cc3cd95b2d8b41180a19fe30ca
kayraobi <kayrayilmaz203@gmail.com>
2025-02-14 00:37:33 +0100

    Formatting adjusted

--- include/NeuralNetwork.hpp
+++ include/NeuralNetwork.hpp
@@ -2,19 +2,14 @@
 #include "Matrix.hpp"
 #include "Layer.hpp"
 
+class NeuralNetwork
+{
+public:
+    NeuralNetwork(vector<int> topology);
 
-class NeuralNetwork{
-
- public:
- NeuralNetwork(vector<int> topology);
- private:
- int topologySize;
- vector<int> topology;
- vector<Layer *> layers;
- vector<Matrix *> weightMatrices;
-
-
-
-
-
+private:
+    int topologySize;
+    vector<int> topology;
+    vector<Layer *> layers;
+    vector<Matrix *> weightMatrices;
 };
--- src/Matrix.cpp
+++ src/Matrix.cpp
@@ -4,66 +4,66 @@
 
 Matrix *Matrix::transpose()
 {
-	Matrix *m = new Matrix(this->numCols, this->numRows, false);
-	for (int i = 0; i < numRows; i++)
-	{
-		for (int j = 0; j < numCols; j++)
-		{
-			m->setValue(j, i, this->getValue(i, j));
-		}
-	}
-	return m;
+    Matrix *m = new Matrix(this->numCols, this->numRows, false);
+    for (int i = 0; i < numRows; i++)
+    {
+        for (int j = 0; j < numCols; j++)
+        {
+            m->setValue(j, i, this->getValue(i, j));
+        }
+    }
+    return m;
 }
 
 void Matrix::setValue(int r, int c, double v)
 {
-	this->values.at(r).at(c) = v;
+    this->values.at(r).at(c) = v;
 }
 
 double Matrix::getValue(int r, int c)
 {
-	return this->values.at(r).at(c);
+    return this->values.at(r).at(c);
 }
 
 double Matrix::generateRandomNumber()
 {
-	std::random_device rd;
-	std::mt19937 gen(rd());
-	std::uniform_real_distribution<double> dis(0.0, 1.0);
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_real_distribution<double> dis(0.0, 1.0);
 
-	return dis(gen);
+    return dis(gen);
 }
 
 void Matrix::printMatrix()
 {
-	for (int i = 0; i < numRows; i++)
-	{
-		for (int j = 0; j < numCols; j++)
-		{
-			std::cout << this->values.at(i).at(j) << "\t";
-		}
-		std::cout << std::endl;
-	}
+    for (int i = 0; i < numRows; i++)
+    {
+        for (int j = 0; j < numCols; j++)
+        {
+            std::cout << this->values.at(i).at(j) << "\t";
+        }
+        std::cout << std::endl;
+    }
 }
 
 Matrix::Matrix(int numRows, int numCols, bool isRandom)
 {
-	this->numRows = numRows;
-	this->numCols = numCols;
+    this->numRows = numRows;
+    this->numCols = numCols;
 
-	for (int i = 0; i < numRows; i++)
-	{
-		vector<double> colValues;
-		for (int j = 0; j < numCols; j++)
-		{
-			double r = 0.00;
-			if (isRandom)
-			{
-				r = this->generateRandomNumber();
-			}
-			colValues.push_back(r);
-		}
+    for (int i = 0; i < numRows; i++)
+    {
+        vector<double> colValues;
+        for (int j = 0; j < numCols; j++)
+        {
+            double r = 0.00;
+            if (isRandom)
+            {
+                r = this->generateRandomNumber();
+            }
+            colValues.push_back(r);
+        }
 
-		this->values.push_back(colValues);
-	}
+        this->values.push_back(colValues);
+    }
 }
